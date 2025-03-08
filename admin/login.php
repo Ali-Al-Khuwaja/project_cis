a96a0baadb6cd5678c67d18ed6b06b3b10aee477
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2025-03-09 00:24:44 +0300

    Set up pages for admin

---
+++ admin/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.pumpkin.min.css">    
+    <meta charset="UTF-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>تسجيل دخول المدير</title> 
+</head>
+<body>
+
+    <main class="container">
+        <article>
+            <header>
+                <h1>تسجيل دخول المدير</h1>
+            </header>
+
+            <div>
+                <p>يرجى إدخال بيانات الاعتماد لتسجيل الدخول إلى لوحة التحكم.</p>
+                </div>
+
+        </article>
+    </main>
+
+</body>
+</html>
